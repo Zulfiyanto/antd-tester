8c642e96bdb805225ece37b097b40f1d691851a8
zulfiyanto <zulfikar@ilcs.co.id>
2024-06-26 12:58:57 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
+  );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,16 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/about')) {
-    return NextResponse.rewrite(new URL('/about-2', request.url))
+  const auth = '';
+
+  if (!auth) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
- 
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.rewrite(new URL('/dashboard/user', request.url))
-  }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/dashbo', '/'],
+};
